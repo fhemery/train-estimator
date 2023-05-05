2a5e3043144f0f82694e89f2f7e5366ba8a4bad2
CorentinGarnier <corentin.garnier40@gmail.com>
2023-05-05 10:15:04 +0200

    refactoring

--- ts/src/train-estimator.ts
+++ ts/src/train-estimator.ts
@@ -20,94 +20,94 @@ export class TrainTicketEstimator {
         }
 
         // TODO USE THIS LINE AT THE END
-        const b = (await(await fetch(`https://sncf.com/api/train/estimate/price?from=${trainDetails.details.from}&to=${trainDetails.details.to}&date=${trainDetails.details.when}`)).json())?.price || -1;
+        const fetchPrice = (await(await fetch(`https://sncf.com/api/train/estimate/price?from=${trainDetails.details.from}&to=${trainDetails.details.to}&date=${trainDetails.details.when}`)).json())?.price || -1;
 
-        if (b === -1) {
+        if (fetchPrice === -1) {
             throw new ApiException();
         }
 
-        const pasngers = trainDetails.passengers;
-        let tot = 0;
-        let tmp = b;
-        for (let i=0;i<pasngers.length;i++) {
+        const pasengers = trainDetails.passengers;
+        let totalPrice = 0;
+        let priceBeforeDiscount = fetchPrice;
+        for (let i=0;i<pasengers.length;i++) {
 
-            if (pasngers[i].age < 0) {
+            if (pasengers[i].age < 0) {
                 throw new InvalidTripInputException("Age is invalid");
             }
-            if (pasngers[i].age < 1) {
-                tmp = 0;
+            if (pasengers[i].age < 1) {
+                priceBeforeDiscount = 0;
             }
             // Seniors
-            else if (pasngers[i].age <= 17) {
-            tmp = b* 0.6;
-            } else if(pasngers[i].age >= 70) {
-                tmp = b * 0.8;
-                if (pasngers[i].discounts.includes(DiscountCard.Senior)) {
-                    tmp -= b * 0.2;
+            else if (pasengers[i].age <= 17) {
+                priceBeforeDiscount = fetchPrice * 0.6;
+            } else if(pasengers[i].age >= 70) {
+                priceBeforeDiscount = fetchPrice * 0.8;
+                if (pasengers[i].discounts.includes(DiscountCard.Senior)) {
+                    priceBeforeDiscount -= fetchPrice * 0.2;
                 }
             } else {
-                tmp = b*1.2;
+                priceBeforeDiscount = fetchPrice*1.2;
             }
 
-            const d = new Date();
-            if (trainDetails.details.when.getTime() >= d.setDate(d.getDate() +30)) {
-                tmp -= b * 0.2;
-            } else if (trainDetails.details.when.getTime() > d.setDate(d.getDate() -30 + 5)) {
+            const date = new Date();
+            if (trainDetails.details.when.getTime() >= date.setDate(date.getDate() +30)) {
+                priceBeforeDiscount -= fetchPrice * 0.2;
+            } else if (trainDetails.details.when.getTime() > date.setDate(date.getDate() -30 + 5)) {
                 const date1 = trainDetails.details.when;
                 const date2 = new Date();
                 //https://stackoverflow.com/questions/43735678/typescript-get-difference-between-two-dates-in-days
-                var diff = Math.abs(date1.getTime() - date2.getTime());
-                var diffDays = Math.ceil(diff / (1000 * 3600 * 24));
+                const diffDates = Math.abs(date1.getTime() - date2.getTime());
+                const diffDays = Math.ceil(diffDates / (1000 * 3600 * 24));
 
-                tmp += (20 - diffDays) * 0.02 * b; // I tried. it works. I don't know why.
+                priceBeforeDiscount += (20 - diffDays) * 0.02 * fetchPrice; // I tried. it works. I don't know why.
             } else {
-                tmp += b;
+                priceBeforeDiscount += fetchPrice;
             }
 
-            if (pasngers[i].age > 0 && pasngers[i].age < 4) {
-                tmp = 9;
+            if (pasengers[i].age > 0 && pasengers[i].age < 4) {
+                priceBeforeDiscount = 9;
             }
 
-            if (pasngers[i].discounts.includes(DiscountCard.TrainStroke)) {
-                tmp = 1;
+            if (pasengers[i].discounts.includes(DiscountCard.TrainStroke)) {
+                priceBeforeDiscount = 1;
             }
 
-            tot += tmp;
-            tmp = b;
+            totalPrice += priceBeforeDiscount;
+            priceBeforeDiscount = fetchPrice;
         }
 
-        if (pasngers.length == 2) {
-            let cp = false;
-            let mn = false;
-            for (let i=0;i<pasngers.length;i++) {
-                if (pasngers[i].discounts.includes(DiscountCard.Couple)) {
-                    cp = true;
+        if (pasengers.length == 2) {
+            let couple = false;
+            let minor = false;
+            for (let i=0;i<pasengers.length;i++) {
+                if (pasengers[i].discounts.includes(DiscountCard.Couple)) {
+                    couple = true;
                 }
-                if (pasngers[i].age < 18) {
-                    mn = true;
-                }
-            }
-            if (cp && !mn) {
-                tot -= b * 0.2 * 2;
-            }
-        }
-
-        if (pasngers.length == 1) {
-            let cp = false;
-            let mn = false;
-            for (let i=0;i<pasngers.length;i++) {
-                if (pasngers[i].discounts.includes(DiscountCard.HalfCouple)) {
-                    cp = true;
-                }
-                if (pasngers[i].age < 18) {
-                    mn = true;
+                if (pasengers[i].age < 18) {
+                    minor = true;
                 }
             }
-            if (cp && !mn) {
-                tot -= b * 0.1;
+            if (couple && !minor) {
+                totalPrice -= fetchPrice * 0.2 * 2;
             }
         }
 
-        return tot;
+        if (pasengers.length == 1) {
+            let midCouple = false;
+            let minor = false;
+            for (let i=0;i<pasengers.length;i++) {
+                if (pasengers[i].discounts.includes(DiscountCard.HalfCouple)) {
+                    midCouple = true;
+                }
+                if (pasengers[i].age < 18) {
+                    minor = true;
+                }
+            }
+            if (midCouple && !minor) {
+                totalPrice -= fetchPrice * 0.1;
+            }
+        }
+
+        return totalPrice;
     }
 }
