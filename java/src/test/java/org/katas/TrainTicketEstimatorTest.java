f9606452edb6b8f30ff333f396ac3df57029f03d
fhemery <f.hemery@hemit.fr>
2024-06-24 11:35:51 +0200

    Add starter test for Java

---
+++ java/src/test/java/org/katas/TrainTicketEstimatorTest.java
@@ -0,0 +1,13 @@
+package org.katas;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class TrainTicketEstimatorTest {
+
+    @Test
+    void should_NotWork() {
+        assertEquals(4, 1+2);
+    }
+  
+}
